bf6a768397e203bf8ad8df63042419631a8ad249
ifiyemiO <ifiyemi.owi@keyin.com>
2024-10-04 01:44:24 -0230

    Initial commit

--- src/main/java/com/keyin/Book.java
+++ src/main/java/com/keyin/Book.java
@@ -1,8 +1,8 @@
 package com.keyin;
 
 public class Book {
-    private String title;
-    private String author;
+    private final String title;
+    private final String author;
     private boolean isAvailable;
 
     public Book(String title, String author) {
